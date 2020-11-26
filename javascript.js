224e095e6a0230425852241a0cd39340fa898c7c
Tony Coder <59034553+tony-the-coder@users.noreply.github.com>
2020-11-26 13:48:20 -0500

    Add the JS. Early view before adding cleanerr CSS.

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const newLocal = "Dec 25, 2020 00:00:00";
+let countDownDate = new Date(newLocal).getTime();
+
+let x = setInterval(function () {
+
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+
+    //The calculations for days hours minutes seconds
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML =
+        days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
